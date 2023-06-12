43faa4a715746cc12f7d5a5badacbefc65aed36a
Kevin Bravo <kbravo1024@gmail.com>
2023-06-11 19:07:16 -0700

    partial complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Directors Page</h1>
+
+      {directors.map((director, index) => {
+        return (
+          <div key={index}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map((movie,index) => {
+                return <li key={index}>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Movies Page</h1>
+
+      {movies.map((movie, index) => {
+        return (
+          <div key={index}>
+            <h2>
+              {movie.title}-{movie.time} minutes long
+            </h2>
+            <ul>
+              {movie.genres.map((genre, index) => {
+                return <li key={index}>{genre}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Movies;
